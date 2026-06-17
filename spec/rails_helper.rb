d9679a1e6f9d5f4ee8503a801c3532f6b04bcdc7
shosem <shousei4794@icloud.com>
2026-06-17 20:16:12 +0900

    Update: RSpecでdeviseをinclude
    deviseのメソッドを使うためinclude

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,6 +59,8 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
