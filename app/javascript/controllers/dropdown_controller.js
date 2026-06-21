26504d98cb01ae74f84c6c0185a11e691c0afe5a
shosem <shousei4794@icloud.com>
2026-06-21 15:44:03 +0900

    Add: ドロップダウン用のsthimulusコントローラ追加
    drpodown_controller.jsを追加し、index.jsに登録

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+
+  static targets = [ "menu" ]
+  toggle() {
+    this.menuTarget.classList.toggle('hidden')
+  }
+
+  hide(event) {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.classList.add('hidden')
+    }
+  }
+}
