7182bf270defe2f5be59da25b92a6529d9f595ae
shosem <shousei4794@icloud.com>
2026-06-22 18:19:49 +0900

    Update: Group - User間のアソシエーション追加
    両モデルにアソシエーションを追加
    また、Groupにはバリデーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 40 }
+  has_many :groups, dependent: :destroy
 end
