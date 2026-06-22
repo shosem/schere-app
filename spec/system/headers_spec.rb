91fad517bf708c980cdc0a945b35976aa08f5083
shosem <shousei4794@icloud.com>
2026-06-22 12:41:41 +0900

    Add: ヘッダーのシステムスペックの追加

---
+++ spec/system/headers_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe "Headers", type: :system do
+
+  include LoginMacros
+
+  describe "ログイン状態によるヘッダーの画面表示" do
+  
+    context "ログイン時" do
+
+      let(:user) { create(:user) }
+
+      before do
+        login(user)
+      end
+
+      it "名前の頭文字のアイコンが表示されること" do
+        expect(page).to have_button(user.name.first)
+        expect(page).to have_no_link("ログイン")
+      end
+
+      it "名前の頭文字のアイコンをクリックし、開いたドロップダウンメニューからログアウトできること" do
+        click_button(user.name.first)
+        click_on("ログアウト")
+        expect(page).to have_current_path(root_path)
+        expect(page).to have_content("ログアウトしました")
+        expect(page).to have_link("ログイン")
+      end
+    end
+
+    context "未ログイン時" do
+      before do
+        visit root_path
+      end
+
+      it "新規登録とログインのリンクが表示されていること" do
+        expect(page).to have_link("新規登録")
+        expect(page).to have_link("ログイン")
+      end
+
+      it "新規登録画面に遷移できること" do
+        click_link "新規登録"
+        expect(page).to have_current_path(new_user_registration_path)
+      end
+
+      it "ログイン画面に遷移できること" do
+        click_link "ログイン", match: :first
+        expect(page).to have_current_path(new_user_session_path)
+      end
+    end
+  end
+end
