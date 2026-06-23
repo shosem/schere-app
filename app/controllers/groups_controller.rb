18abb770a7764b183cade1c51daea2a570cf6d37
shosem <shousei4794@icloud.com>
2026-06-23 11:58:38 +0900

    Add: groupsコントローラの作成
    index new createアクション定義

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+  
+  def index
+    @groups = current_user.groups
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.build(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      flash.now[:danger] = "グループを作成できませんでした"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
