574e972c01b14a6c8df87964732737f4b8dc00c9
shosem <shousei4794@icloud.com>
2026-06-24 16:14:37 +0900

    Update: header_specの修正
    認証画面にヘッダーを表示しないようにした修正に伴い、失敗となったエラーを修正
    また、認証画面にヘッダーが表示されないテストを追加

--- spec/system/headers_spec.rb
+++ spec/system/headers_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe "Headers", type: :system do
   include LoginMacros
 
   describe "ログイン状態によるヘッダーの画面表示と遷移" do
+    let(:user) { create(:user) }
     context "ログイン時" do
-      let(:user) { create(:user) }
-
       before do
         login(user)
       end
@@ -21,7 +20,6 @@ RSpec.describe "Headers", type: :system do
         click_on("ログアウト")
         expect(page).to have_current_path(new_user_session_path)
         expect(page).to have_content("ログアウトしました")
-        expect(page).to have_link("ログイン")
       end
     end
 
@@ -30,19 +28,21 @@ RSpec.describe "Headers", type: :system do
         visit root_path
       end
 
-      it "新規登録とログインのリンクが表示されていること" do
-        expect(page).to have_link("新規登録")
-        expect(page).to have_link("ログイン")
-      end
-
-      it "新規登録画面に遷移できること" do
-        click_link "新規登録"
-        expect(page).to have_current_path(new_user_registration_path)
-      end
-
-      it "ログイン画面に遷移できること" do
-        click_link "ログイン", match: :first
+      it "ログイン画面に遷移すること" do
         expect(page).to have_current_path(new_user_session_path)
+        expect(page).to have_content("ログイン")
+      end
+
+      it "ログイン画面でヘッダーが表示されていないこと" do
+        visit new_user_session_path
+        expect(page).to have_no_link("Schere")
+        expect(page).to have_no_link(new_user_session_path)
+      end
+
+      it "新規登録画面でヘッダーが表示されていないこと" do
+        visit new_user_registration_path
+        expect(page).to have_no_link("Schere")
+        expect(page).to have_no_button(user.name.first)
       end
     end
   end
