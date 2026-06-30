f3cf0f042488d65efcc90f0a9e2df241ec6b569a
shosem <shousei4794@icloud.com>
2026-06-30 12:04:09 +0900

    Add: アクションの追加 new_by_token、create_by_tokenアクションを追加

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,2 +1,21 @@
 class GuestSessionsController < ApplicationController
+  before_action :set_group
+
+  def new_by_token
+    @guest = Guest.new
+  end
+
+  def create_by_token
+    @guest = Guest.find_or_initialize_by(group_id: @group.id, name: params[:guest][:name])
+    @guest.save! if @guest.new_record?
+
+    session[:guest_token] = @guest.session_token
+    redirect_to group_path(@group), notice: "#{@group.name}に#{@guest.name}さんとして入室しました"
+  end
+
+  private
+
+  def set_group
+    @group = Group.find_by!(join_token: params[:join_token])
+  end
 end
