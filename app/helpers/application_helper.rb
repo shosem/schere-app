841fb5e68444458a95a3a013f1f32d56a50d88ee
shosem <shousei4794@icloud.com>
2026-07-01 17:27:47 +0900

    Add: ゲスト入室か否か、でロゴのリンク（タグ自体）を切り替えるメソッドを追加
    ゲスト入室時には<span>にし、それ以外ではlink_toでroot_pathへ飛ぶように

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def current_guest
-    @current_guest ||= Guest.find_by(session_token: session[:guest_token])
+  def header_logo
+    if current_guest
+      tag.span("Schere", class: "text-xl font-bold text-blue-500")
+    else
+      link_to "Schere", root_path, class: "text-xl font-bold text-blue-500"
+    end
   end
 end
