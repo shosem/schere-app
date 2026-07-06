9572a08e6fd9c15db3d3edb8bcf41402a005556f
shosem <shousei4794@icloud.com>
2026-07-06 16:27:48 +0900

    Update: マイグレーションファイルの編集
    eventsテーブルの各カラムを編集。未マイグレーション

--- db/migrate/20260706070347_create_events.rb
+++ db/migrate/20260706070347_create_events.rb
@@ -1,6 +1,13 @@
 class CreateEvents < ActiveRecord::Migration[8.1]
   def change
     create_table :events do |t|
+      t.references :group, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.string :title, null: false
+      t.text :description
+      t.integer :status, null: false, default: 0
+      t.bigint :confirmed_candidate_date_id
+      t.string :location
       t.timestamps
     end
   end
