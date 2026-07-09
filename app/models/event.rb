f7e0ecefa3971c659a3d8470402711e3543ede47
shosem <shousei4794@icloud.com>
2026-07-09 13:44:07 +0900

    Update: confirmed_candidate_date_idの関連付けを追加
    event.rbに、confirmed_candidate_date_idが、Candidate_dateを参照するよう記載

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :group
   has_many :candidate_dates, dependent: :destroy
+  belongs_to :confirmed_candidate_date,
+             class_name: "CandidateDate",
+             optional: true
   accepts_nested_attributes_for :candidate_dates, allow_destroy: true
   enum :status, { adjusting: 0, confirmed: 1, cancelled: 2 }, default: :adjusting
 
