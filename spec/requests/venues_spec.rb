a6e14e004e395f59659661cca8650c75401ae3b0
shosem <shousei4794@icloud.com>
2026-07-22 18:19:49 +0900

    Add: リクエストスペック追加
    また実際のviewはないので、リクエストが遂行されるかをテスト

---
+++ spec/requests/venues_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "Venues", type: :request do
+  describe "venues" do
+    let(:user) { create(:user) }
+    let(:group) { create(:group, user: user) }
+    let(:event) { create(:event, group: group, user: user) }
+    before do
+      sign_in user
+    end
+
+    it "予約情報を作成できること" do
+      expect { post group_event_venues_path(group, event), params: {
+        venue: { name: "居酒屋"}
+        }
+      }.to change(Venue, :count).by(1)
+      expect(response).to redirect_to(group_event_path(group, event))
+    end
+
+    it "予約情報を更新できること" do
+      venue = create(:venue, event: event)
+      patch group_event_venue_path(group, event, venue), params: {
+        venue: { name: "修正後" }
+      }
+      expect(response).to redirect_to(group_event_path(group, event))
+      expect(venue.reload.name).to eq "修正後"
+    end
+
+    it "予約情報を削除できること" do
+      venue = create(:venue, event: event)
+      expect { delete group_event_venue_path(group, event, venue)
+      }.to change(Venue, :count).by(-1)
+      expect(Venue.count).to eq 0
+    end
+  end
+end
