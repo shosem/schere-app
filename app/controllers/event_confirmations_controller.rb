3d7a1673ac3fe1ad2fe1422a848a3735a3775d32
shosem <shousei4794@icloud.com>
2026-07-24 14:51:04 +0900

    Add: update destroyアクション追加
    update（確定済みにする）、destroy（調整中に戻す）を追加。
    destroyの動線はステータスによる出し分けを編集する際に追加する

--- app/controllers/event_confirmations_controller.rb
+++ app/controllers/event_confirmations_controller.rb
@@ -1,2 +1,29 @@
 class EventConfirmationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_group
+  before_action :set_event
+  def update
+
+    @candidate_date = @event.candidate_dates.find(params[:candidate_date_id])
+
+    @event.update!(confirmed_candidate_date_id: @candidate_date.id, status: :confirmed)
+    redirect_to group_event_path(@group, @event), notice: "日程を確定しました"
+  end
+
+  def destroy
+    @event.update!(confirmed_candidate_date_id: nil, status: :adjusting)
+    redirect_to group_event_path(@group, @event), notice: "調整中に戻しました"
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+    # authorized?はユーザーログインならオーナー判定をしてる
+    deny_access unless authorized?(@group)
+  end
+
+  def set_event
+    @event = @group.events.find(params[:event_id])
+  end
 end
