5276ed1fb001dd5ff5009f115372855dfe3169fd
shosem <shousei4794@icloud.com>
2026-07-31 20:08:16 +0900

    Add: モーダルを操作するstimulusコントローラを追加 index.jsに登録 関数は書きおえた時点

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -13,5 +13,8 @@ application.register("dropdown", DropdownController)
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import ModalController from "./modal_controller"
+application.register("modal", ModalController)
+
 import ShareController from "./share_controller"
 application.register("share", ShareController)
