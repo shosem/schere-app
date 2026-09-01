91707a2e09ed2c25a82984d51c86ec3b19d11994
shosem <shousei4794@icloud.com>
2026-09-01 22:13:06 +0900

    Add: 旧ドメインへのgetリクエストを新ドメインにリダイレクトさせる
    getのみに絞っているのは、post、patch、deleteは基本その前にgetを行なっているため。
    fullpathメソッドを用いて、パスからクエリパラメータまで引き継いで新ドメインへリダイレクトさせる。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  constraints host: 'schere-app.onrender.com' do
+    get '/(*path)', to: redirect { |_params, req| "https://schere.shosem.com#{req.fullpath}" }
+  end
   devise_for :users, controllers:
     { registrations: "users/registrations"
   }
